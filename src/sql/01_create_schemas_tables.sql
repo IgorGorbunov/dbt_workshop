73e84a4cd455908c7d760bec28363048aabdacdf
Горбунов Игорь Вадимович <gorbunov@polymedia.ru>
2025-01-08 02:34:22 +0400

    add sql init scripts

---
+++ src/sql/01_create_schemas_tables.sql
@@ -0,0 +1,35 @@
+create schema if not exists jaffle_shop;
+create schema if not exists stripe;
+create schema if not exists dev;
+
+create table jaffle_shop.customers(
+    id integer,
+    first_name varchar(50),
+    last_name varchar(50)
+);
+
+create table jaffle_shop.payments(
+    id integer, 
+    order_id integer, 
+    payment_method text, 
+    status text, 
+    amount integer, 
+    created date
+);
+
+
+create table jaffle_shop.orders(
+    id integer,
+    user_id integer,
+    order_date date,
+    status varchar(50)
+);
+
+create table stripe.payment(
+    id integer,
+    orderid integer,
+    paymentmethod varchar(50),
+    status varchar(50),
+    amount integer,
+    created date
+);
